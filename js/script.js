962a8019724634b54bc7bff69599d4f73228d8f4
Jakub Nowak <kubanowak10@vp.pl>
2025-03-06 21:16:35 +0100

    Naprawiono ścieżki do plików JS i CSS

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var computerMove;
+computerMove = 'papier';
+
+var playerMove;
+playerMove = 'papier';
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+printMessage('Twój ruch: ' + playerMove);
