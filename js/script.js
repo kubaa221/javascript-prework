e134c587ed0119783ec5bc5405b859f0004cf45b
Jakub Nowak <kubanowak10@vp.pl>
2025-03-08 21:21:04 +0100

    Dodano wybór ruchu przez gracza za pomocą prompt()

--- js/script.js
+++ js/script.js
@@ -15,3 +15,24 @@ if (randomNumber === 1) {
 
 // Wyświetlenie wylosowanego ruchu
 printMessage('Komputer zagrał: ' + computerMove);
+// Pobieranie ruchu gracza
+var playerInput = prompt('Wybierz swój ruch! Wpisz 1 dla kamień, 2 dla papier, 3 dla nożyce:');
+
+console.log('Wpisana odpowiedź:', playerInput);
+
+var playerMove;
+
+// Instrukcja warunkowa przypisująca ruch gracza
+if (playerInput === '1') {
+    playerMove = 'kamień';
+} else if (playerInput === '2') {
+    playerMove = 'papier';
+} else if (playerInput === '3') {
+    playerMove = 'nożyce';
+} else {
+    playerMove = 'nieznany ruch';
+    printMessage('Błędny wybór! Wpisz 1, 2 lub 3.');
+}
+
+// Wyświetlenie ruchu gracza
+printMessage('Twój ruch: ' + playerMove);
