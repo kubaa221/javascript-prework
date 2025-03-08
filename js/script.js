57a62b2a282824ba92b9b6e34f74093c76ef456d
Jakub Nowak <kubanowak10@vp.pl>
2025-03-08 20:55:11 +0100

    Dodano losowanie liczb 1-3 oraz 11-19

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-var computerMove;
-computerMove = 'papier';
-
-var playerMove;
-playerMove = 'papier';
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
-printMessage('Twój ruch: ' + playerMove);
+// Losowanie liczby od 1 do 3
+var randomNumber = Math.floor(Math.random() * 3) + 1;
+printMessage('Wylosowana liczba: ' + randomNumber);
+// Losowanie liczby od 11 do 19
+var randomNumber2 = Math.floor(Math.random() * 9) + 11;
+printMessage('Wylosowana liczba (11-19): ' + randomNumber2);
