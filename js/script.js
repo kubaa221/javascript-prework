8b914049b81480078db656fad9e00fde1334c158
Jakub Nowak <kubanowak10@vp.pl>
2025-03-08 21:14:19 +0100

    Dodano logikę if/else do wyboru ruchu komputera

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 // Losowanie liczby od 1 do 3
 var randomNumber = Math.floor(Math.random() * 3) + 1;
-printMessage('Wylosowana liczba: ' + randomNumber);
-// Losowanie liczby od 11 do 19
-var randomNumber2 = Math.floor(Math.random() * 9) + 11;
-printMessage('Wylosowana liczba (11-19): ' + randomNumber2);
+console.log('Wylosowana liczba:', randomNumber);
+
+var computerMove; // Zmienna przechowująca ruch komputera
+
+// Instrukcja warunkowa przypisująca ruch
+if (randomNumber === 1) {
+    computerMove = 'kamień';
+} else if (randomNumber === 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
+
+// Wyświetlenie wylosowanego ruchu
+printMessage('Komputer zagrał: ' + computerMove);
